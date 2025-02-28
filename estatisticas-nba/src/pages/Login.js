0db78c88e5fa84c6ddbcca5ae234c839811fecfd
Fábio Cardoso <fabiotarcisioccpa@gmail.com>
2025-02-28 10:47:08 -0300

    Criei estado de usuário e senha

--- estatisticas-nba/src/pages/Login.js
+++ estatisticas-nba/src/pages/Login.js
@@ -2,11 +2,33 @@ import React from "react";
 
 
 class Login extends React.Component {
+
+ state = {
+    user : '',
+    password: '',
+  };
+
+  user_state_change = ({ target }) => {
+    const user_value = target.value;
+    this.setState(() => ( {
+      user : user_value,
+    } ));
+    console.log(user_value);
+  }
+
+  password_state_change = ({ target }) => {
+    const password_value = target.value;
+    this.setState(() => ({
+      password : password_value,
+    } ));
+    console.log(password_value);
+  }
+
   render(){
     return(
     <div>
-      <input placeholder="Usuário"></input>
-      <input placeholder="Senha" type="password"></input>
+      <input placeholder="Usuário" onChange={this.user_state_change}></input>
+      <input placeholder="Senha" type="password" onChange={this.password_state_change}></input>
       <button></button>
     </div>
     )
