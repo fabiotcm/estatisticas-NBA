eba694f7bdf77e0fd37d17eb45de742bf62ced23
Levy Araujo <levy4raujo@gmail.com>
2025-02-28 11:24:37 -0300

    [CSS] Criando CSS para Login.

--- estatisticas-nba/src/pages/Login.js
+++ estatisticas-nba/src/pages/Login.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Logo from "../components/Logo";
+import "./css/pages.css"
 
 class Login extends React.Component {
   render(){
     return(
-    <div>
+    <div className="flex-box">
       <Logo/>
       <input placeholder="Usuário"></input>
       <input placeholder="Senha" type="password"></input>
