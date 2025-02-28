8088a27cf919221d1823fe696469bd3a51f32a5e
Levy Araujo <levy4raujo@gmail.com>
2025-02-28 10:28:38 -0300

    [CSS] Criando componente logo e adicionando ao Login.

--- estatisticas-nba/src/pages/Login.js
+++ estatisticas-nba/src/pages/Login.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Logo from "../components/Logo";
 
 class Login extends React.Component {
   render(){
     return(
     <div>
+      <Logo/>
       <input placeholder="Usuário"></input>
       <input placeholder="Senha" type="password"></input>
     </div>
