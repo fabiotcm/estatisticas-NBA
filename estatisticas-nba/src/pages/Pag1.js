29f1fb801931b377a8c7939550d3feef095db6fc
Fábio Cardoso <fabiotarcisioccpa@gmail.com>
2025-03-06 10:41:54 -0300

    Consumida a API NBA stats

--- estatisticas-nba/src/pages/Pag1.js
+++ estatisticas-nba/src/pages/Pag1.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Link, Redirect } from "react-router-dom";
+import jogadores_por_temp from "../services/nbaAPI";
+
+console.log(jogadores_por_temp());
 
 class Pag1 extends React.Component{
   render(){
--- estatisticas-nba/src/services/nbaAPI.js
+++ estatisticas-nba/src/services/nbaAPI.js
@@ -1,2 +1,18 @@
+const jogadores_por_temp = async () => {
 
+  var myHeaders = new Headers();
+  myHeaders.append("x-rapidapi-key", "b8a174623e936260e0c84d01701daa66");
+  myHeaders.append("x-rapidapi-host", "v2.nba.api-sports.io");
 
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+
+  const request = await fetch('https://v2.nba.api-sports.io/players/statistics?season=2022&id=734', requestOptions);
+  const requestJSON = await request.json();
+  return requestJSON
+}
+
+export default jogadores_por_temp;
