42199baaf35642328a137fbc108c587178be9bae
Fábio Cardoso <fabiotarcisioccpa@gmail.com>
2025-03-07 11:53:21 -0300

    Adicionado todos os times e logos na pagina 1

--- estatisticas-nba/src/pages/Pag1.js
+++ estatisticas-nba/src/pages/Pag1.js
@@ -1,26 +1,47 @@
 import React from "react";
-import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 import times_nba from "../services/nbaAPI";
 
 
 
-const lista_times = async () => {
-  const todos_times = await times_nba();
-  const {response} = todos_times;
-  let lista_times_nba = [];
-  for (let index = 0; index < response.length; index++) {
-    if (response[index].nbaFranchise) {
-      lista_times_nba.push(response[index]);
-    }
-  }
-  return lista_times_nba;
-}
+
 
 class Pag1 extends React.Component{
+  
+  state = {
+    times : [],
+  };
+  
+  componentDidMount() {
+    this.lista_times();
+  }
+
+  lista_times = async () => {
+    const todos_times = await times_nba();
+    const {response} = todos_times;
+    let lista_times_nba = response.filter((x) => x.nbaFranchise);
+    this.setState(() => ({
+      times : lista_times_nba
+    }))
+  }
+
+  
   render(){
-    console.log(lista_times());
+    const {times} = this.state;
+    const array_times = times.map((time) => {
+      const logo_time = time.logo;
+      const nome_time = time.name;
+      return (<div key={nome_time}>
+        <img src={logo_time} alt={nome_time}/>
+        <div>{nome_time}</div>
+      </div>)
+    })
     return(
-      <Link to='/Pag2'>Pag2</Link>
+      <div>
+        <Link to='/Pag2'>Pag2</Link>
+        {array_times}
+      </div>
+      
     )
   }
 }
