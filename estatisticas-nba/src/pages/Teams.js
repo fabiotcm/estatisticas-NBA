05236a1e8b7fd001745e300692e72d70a3b6bd6e
Fábio Cardoso <fabiotarcisioccpa@gmail.com>
2025-03-10 11:29:42 -0300

    Fiz uma tela de carregamento entre login e teams

--- estatisticas-nba/src/pages/Teams.js
+++ estatisticas-nba/src/pages/Teams.js
@@ -7,6 +7,7 @@ class Teams extends React.Component{
   
   state = {
     times : [],
+    carregando : true,
   };
   
   componentDidMount() {
@@ -18,27 +19,38 @@ class Teams extends React.Component{
     const {response} = todos_times;
     let lista_times_nba = response.filter((x) => x.nbaFranchise);
     this.setState(() => ({
-      times : lista_times_nba
+      times : lista_times_nba,
+      carregando : false,
     }))
+
   }
 
   
   render(){
     const {times} = this.state;
+    const {carregando} = this.state;
     const array_times = times.map((time) => {
       return (
         <div className="team-card" key={time.name}>
           <img src={time.logo} alt={time.name}/>
         </div>)
     })
-    return(
-      <div>
-        <div className="team-box">
-          {array_times}
-        </div>
-      </div>
-      
-    )
+    
+    if (carregando){
+      return(
+        <div>Carregando</div>
+      )
+    }else{
+      return(
+          <div>
+            <div className="team-box">
+              {array_times}
+            </div>
+          </div>
+      )
+
+    }
+    
   }
 }
 
