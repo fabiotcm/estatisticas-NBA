664b986eaeab64ffdae767a0966cca54e55d144d
Fábio Cardoso <fabiotarcisioccpa@gmail.com>
2025-03-22 12:00:02 -0300

    Criada a lista de jogadores por time

--- estatisticas-nba/src/components/PlayerBox.js
+++ estatisticas-nba/src/components/PlayerBox.js
@@ -1,20 +1,51 @@
 import React from "react";
 import NBAContext from "../context/NBAContext";
 import nbaManager from "../services/nbaAPI";
+import { Navigate } from "react-router-dom";
 
 class PlayerBox extends React.Component{
+  state = {
+    lista_jogadores : [],
+    to_player : false,
+  };
 
-  lista_jogadores_time = async (id) => {
+  componentDidMount(){
+    this.lista_jogadores_time();
+  }
+
+  lista_jogadores_time = async () => {
+    const id = this.context.team_id;
     const jogadores = await nbaManager.jogadores_time(id);
-    return jogadores;
+    const {response} = jogadores;
+    this.setState(() => (({
+      lista_jogadores : response,
+    })));
+    
+  }
+
+  go_to_player = (id) =>{
+    this.context.mudarPlayerId(id);
+    this.setState(() =>(({
+      to_player : true,
+    })));
   }
 
   render(){
-    const id = this.context.team_id;
-    const lista_jogadores = this.lista_jogadores_time(id);
-    console.log(lista_jogadores)
+    const {lista_jogadores, to_player} = this.state;
+    console.log(lista_jogadores);
+    const array_jogadores = lista_jogadores.map((jogador) =>{
+      return(
+        <div key={jogador.id} onClick={() => this.go_to_player(jogador.id)}>
+          <div>{jogador.firstname}</div>
+          <div>{jogador.lastname}</div>
+        </div>
+      )
+    });
+    
+    if(to_player) return(<Navigate to='/Player'/>)
+    
     return(
-      <div>{id}</div>
+      <div>{array_jogadores}</div>
     )
   }
 
