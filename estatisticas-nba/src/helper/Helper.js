45539fe62212829d43e9c8f1748e193a30ae7777
Fábio Cardoso <fabiotarcisioccpa@gmail.com>
2025-03-26 10:24:16 -0300

    Retorna nome do jogador, roubos e bloqueios

--- estatisticas-nba/src/helper/Helper.js
+++ estatisticas-nba/src/helper/Helper.js
@@ -3,23 +3,35 @@ const lista_estatisticas_jogador = (jogos) => {
   var total_pontos = 0;
   var total_assists = 0;
   var total_rebotes = 0;
+  var total_bloqueios = 0;
+  var total_roubos = 0;
+  var nome_jogador = '';
 
   jogos.forEach((jogo) => {
-    const {points, assists, totReb} = jogo;
+    const {points, assists, totReb, player, blocks, steals} = jogo;
     
+    nome_jogador = player.firstname + ' ' + player.lastname;
     total_pontos += points;
     total_assists += assists;
     total_rebotes += totReb;
+    total_bloqueios += blocks;
+    total_roubos += steals;
   });
 
   const media_pontos = (total_pontos / numJogos).toFixed(1);
   const media_assists = (total_assists / numJogos).toFixed(1);
   const media_rebotes = (total_rebotes / numJogos).toFixed(1);
+  const media_bloqueios = (total_bloqueios / numJogos).toFixed(1);
+  const media_roubos = (total_roubos / numJogos).toFixed(1);
+  
   
   const tabela = {
-    "Média de pontos": media_pontos,
+    "Jogador": nome_jogador,
+    "Média de Pontos": media_pontos,
     "Média de Assistências": media_assists,
-    "Média de rebotes": media_rebotes,
+    "Média de Rebotes": media_rebotes,
+    "Média de Bloqueios": media_bloqueios,
+    "Média de Roubos": media_roubos,
   };
 
   
