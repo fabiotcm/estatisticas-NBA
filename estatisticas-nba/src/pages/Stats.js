8e4520b38f9a6942065f568c22f899220cab96d3
Levy Araujo <levy4raujo@gmail.com>
2025-03-27 10:24:14 -0300

    [ADD] Fetch Id Team

--- estatisticas-nba/src/pages/Stats.js
+++ estatisticas-nba/src/pages/Stats.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import NBAContext from "../context/NBAContext";
-import nbaManager from "../services/nbaAPI";
 import "../pages/css/stats.css";
 import StatsBox from "../components/StatsBox";
 import PlayerBox from "../components/PlayerBox";
@@ -17,11 +16,6 @@ class Stats extends React.Component {
     this.setState(()=>({
       logo: nba.team_logo,
     }));
-  }
-
-  lista_estatisticas_time = async (id) => {
-    const estatisticas = await nbaManager.estatistica_time(id);
-    return estatisticas;
   }
 
   render(){
